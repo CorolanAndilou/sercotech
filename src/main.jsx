0125895db9f3ace45f5ffed64ae4292a28b19716
Paula <karthus.paula@gmail.com>
2026-06-01 21:35:52 -0400

    Fix: Duplicar estilos en minusculas para evitar fallos de entorno

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/Global.css';
+import './styles/global.css';
 
 
 createRoot(document.getElementById('root')).render(
